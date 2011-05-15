df4e9e52983618df506fc5d0a7bb7d1f18716982
Henrik Brink <henrikbrink@gmail.com>
2011-05-15 23:18:27 +0200

    Added search and get_similar methods to Artist

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -4,6 +4,12 @@ class Lastfm
       regular_method :get_events, [:artist], [] do |response|
         response.xml['events']['event']
       end
+      regular_method :search, [:artist], [] do |response|
+        response.xml['results']['artistmatches']['artist']
+      end
+      regular_method :get_similar, [:artist], [] do |response|
+        response.xml['similarartists']['artist']
+      end
     end
   end
 end
